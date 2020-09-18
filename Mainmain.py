c8cf68a2a29983fd6001c2a751419e9a79d39461
Jason J. W <71363607+digaji@users.noreply.github.com>
2020-09-18 23:11:51 +0700

    third commit?

---
+++ Mainmain.py
@@ -0,0 +1,9 @@
+total = 0.1 + 0.2
+print(total)
+
+# add up all multiples of 3 and 5 from 1 - 100
+total1 = 0
+for i in range(1, 100):
+    if i % 3 == 0 or i % 5 == 0:
+        total1 += i
+print(total1)
