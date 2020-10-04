40c2cba70a4b7f926f961339368089282c5ff1a4
Jason J. W <71363607+digaji@users.noreply.github.com>
2020-10-04 22:28:16 +0700

    deleted files

--- While Loop Challenge.py
+++
@@ -1,9 +0,0 @@
-# Find sum of all negative numbers
-given_list3 = [7, 5, 4, 4, 3, 1, -2, -3, -5, -7]
-total = 0
-i = 0
-while i < len(given_list3):
-    if given_list3[i] < 0:
-        total += given_list3[i]
-    i += 1
-print(total)
